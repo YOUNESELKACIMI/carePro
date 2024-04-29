4e289ff428d250cd5b1c6e9a5e0e911e0f4e7afd
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2024-04-29 23:40:28 +0100

    postgress connection updated

--- accounts/config/db.js
+++ accounts/config/db.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.POSTGRES_PASSWORD,{
+    host: process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+});
 
 module.exports = { sequelize };
